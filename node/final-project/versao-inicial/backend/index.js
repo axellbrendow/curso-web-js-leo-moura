c97283decb41f92ef15e30e031728f00575558fd
axell-brendow <breno.axel@gmail.com>
2020-02-10 19:22:03 -0300

    Create the schema migration for users, categories and articles

--- node/final-project/versao-inicial/backend/index.js
+++ node/final-project/versao-inicial/backend/index.js
@@ -1,9 +1,14 @@
 const app = require('express')();
 const consign = require('consign');
+const db = require('./config/db');
+
+app.db = db;
 
 consign()
     // As middlewares.js exports a function, consign passes the app object to it
     .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app);
 
 const PORT = 3000;
