76164328bb28f480c6026a6ce6a35e3f4aec3368
axell-brendow <breno.axel@gmail.com>
2020-02-10 18:31:25 -0300

    Create first middleware to enable json and cors

--- node/final-project/versao-inicial/backend/index.js
+++ node/final-project/versao-inicial/backend/index.js
@@ -1,4 +1,10 @@
 const app = require('express')();
+const consign = require('consign');
+
+consign()
+    // As middlewares.js exports a function, consign passes the app object to it
+    .then('./config/middlewares.js')
+    .into(app);
 
 const PORT = 3000;
 
