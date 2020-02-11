2937f47f6adb7622436d067b5e66309b925a17d1
axell-brendow <breno.axel@gmail.com>
2020-02-11 19:22:24 -0300

    Create a route to fetch all articles from a category and its child categories

--- node/final-project/versao-inicial/backend/config/routes.js
+++ node/final-project/versao-inicial/backend/config/routes.js
@@ -27,4 +27,7 @@ module.exports = app => {
         .put(app.api.article.save)
         .get(app.api.article.getById)
         .delete(app.api.article.remove);
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory);
 };
